01f194046fa94a7bc07439210f24e6fb5d4e2813
Ash Furrow <ash@ashfurrow.com>
2014-07-26 15:43:26 +0200

    Finished Chapter 2.

--- Coffee Timer/Coffee Timer/ViewController.swift
+++ Coffee Timer/Coffee Timer/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var progressView: UIProgressView!
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +25,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func buttonWasPressed(sender: AnyObject) {
+        println("Button was pressed.")
+        
+        // Get the current date and time
+        let date = NSDate.date()
+        
+        // Update the label
+        label.text = "Button pressed at \(date)"
+    }
+    @IBAction func sliderValueChanged(sender: AnyObject) {
+        println("Slider value changed to \(slider.value)")
+        
+        // Update our progressView's progress to match 
+        // the slider's value
+        progressView.progress = slider.value
+    }
 }
 
